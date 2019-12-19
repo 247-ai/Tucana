4bad1d8816364a450f36012fbfb8daaf095a3ffc
udhay <udhay@Udhay-debian.debian>
2019-12-19 19:10:50 +0530

    Updated the pom file for open source

--- Docker/run-tucana-tests.sh
+++ Docker/run-tucana-tests.sh
@@ -7,4 +7,4 @@ scalaTestJars="scalatest_2.11-3.0.5.jar:scalactic_2.11-3.0.5.jar"
 
 predictTestClass=com.ilabs.dsi.tucana.functionalTests.PredictApiTests
 
-scala -J-Xmx2g -cp "$scalaTestJars:$predictServerMainJar" org.scalatest.tools.Runner -o -R $predictServerTestJar -s $predictTestClass
+scala -J-Xmx2g -cp "$scalaTestJars:$predictServerMainJar" org.scalatest.tools.Runner -o -R $predictServerTestJar -s $predictTestClass || exit -1
